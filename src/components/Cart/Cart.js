51bcc85a1264f0337ff97d7957286031c724d3e6
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-07-23 23:06:14 -0400

    adding in hide / show cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
 
 const Cart = (props) => {
   const cartItems = [{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map(
@@ -8,17 +9,17 @@ const Cart = (props) => {
   );
 
   return (
-    <div className={classes}>
+    <Modal className={classes} onHideCart={props.onHideCart}>
       <ul className={classes["cart-items"]}>{cartItems}</ul>
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button onClick={props.onHideCart} className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
