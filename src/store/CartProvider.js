bc9d3781397c9669e1de1f53487ef9a83c61621b
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-07-28 15:21:05 -0400

    adding in Reducer to track cart state

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,29 +1,53 @@
 import CartContext from "./cart-context";
-import { useState } from "react";
+import { useReducer } from "react";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+// the reducer is outside the component because it doesn't depend on anything inside the component context, and having it outside means it doesn't get re-evaluated with the component, improving speed.
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedArray = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {items: updatedArray,
+    totalAmount: updatedTotalAmount}
+  }
+
+  return defaultCartState;
+};
 
 const CartProvider = (props) => {
-    
-    const addItemToCartHandler = (item) => {};
-  
-    const removeItemFromCartHandler = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
-    const cartContext = {
-        items: [],
-        totalAmount: 0,
-        onAddItem: addItemToCartHandler,
-        onRemoveItem: removeItemFromCartHandler,
-
-    }
-
-//    const [items, setItems] = useState([]);
-  
-    return (
-      <CartContext.Provider
-        value={cartContext}
-      >
-        {props.children}
-      </CartContext.Provider>
-    );
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
   };
 
-  export default CartProvider
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    onAddItem: addItemToCartHandler,
+    onRemoveItem: removeItemFromCartHandler,
+  };
+
+  //    const [items, setItems] = useState([]);
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
