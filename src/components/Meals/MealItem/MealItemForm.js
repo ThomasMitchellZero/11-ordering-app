e0d0f009aa8c5b8da6b23ad354bdc7a3e5f746d6
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-08-06 08:30:20 -0400

    nm

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -2,20 +2,24 @@ import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 
 const MealItemForm = (props) => {
+  const submitHandler = (event) => {
+    event.preventDefault()
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
         label="Amount"
         input={{
-          id: 'amount_' + props.id,
-          type: 'number',
+          id: "amount_" + props.id,
+          type: "number",
           min: "1",
-          max: '5',
-          step: '1',
-          defaultValue: '1',
+          max: "5",
+          step: "1",
+          defaultValue: "1",
         }}
       ></Input>
-      <button>+ Add</button>
+      <button type="submit">+ Add</button>
     </form>
   );
 };
